0ae8d043cb24bbc621f013f60c17ddb917e81ff1
Howard Tyson <howard@howardtyson.com>
2015-03-05 21:39:43 -0500

    Adding help text.

--- bin/amour-config.js
+++ bin/amour-config.js
@@ -5,6 +5,7 @@ var yaml = require('js-yaml');
 var exports = function() {};
 
 exports.shortDescription = 'Displays all loaded configuration.'
+exports.help = 'Specify configuration include paths with `-c` or `--config` and use this command to view the compiled configuration.'
 
 exports.configure = function(config) {
   this.config = config;
