1804d44cf917262d015897b8efd2996777d261dd
Howard Tyson <howard@howardtyson.com>
2015-03-09 16:35:05 -0400

    Adding a stub for amour-proxy.

---
+++ bin/amour-proxy.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var exports = function() {
+  this.configure = this.configure.bind(this);
+  this.run = this.run.bind(this);
+};
+
+exports.shortDescription = '.';
+
+exports.help = 'Runs the API server for creating docker containers.';
+
+exports.options = function(yargs) {
+  yargs
+    .describe('port', 'The port to run on')
+    .alias('port', 'p')
+  ;
+}
+
+module.exports = exports;
