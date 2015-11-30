869cd27126f63a1987f12dad0a0494e80bde755c
Howard Tyson <howard@howardtyson.com>
2015-11-30 05:15:48 -0800

    Updating build test for consistency with coding standards.

--- test/build.js
+++ test/build.js
@@ -1,9 +1,9 @@
-var through2 = require('through2')
+var through2 = require('through2');
 
-var lib = require('..')
-var Build = lib.Build
-var Container = lib.Container
-var Step = require('./fixtures/TaskPlugin')
+var lib = require('..');
+var Build = lib.Build;
+var Container = lib.Container;
+var Step = require('./fixtures/TaskPlugin');
 
 /**
  * @param {string} name - The name of the event to emit.
@@ -18,64 +18,64 @@ Step.prototype.emitEvent = function(name, data) {
  * Create an array of callbacks and call done only once all have been resolved.
  *
  * @param {integer} callbackNumber - The number of callbacks to produce and require.
- * @param {function} done - The callback to call once all 
+ * @param {function} done - The callback to call once all
  * @returns {Array} - An array of functions to be bound to callbacks.
  */
 function resolver(callbackNumber, done) {
-  var callbacks = []
-  var results = []
-  var calledCallbacks = 0
+  var callbacks = [];
+  var results = [];
+  var calledCallbacks = 0;
   for (var i = 0 ; i < callbackNumber ; i++) {
     callbacks.push(function() {
-      results.push(arguments)
-      calledCallbacks++
-      if (calledCallbacks == callbackNumber) {
-        done(null, results)
+      results.push(arguments);
+      calledCallbacks++;
+      if (calledCallbacks === callbackNumber) {
+        done(null, results);
       }
     });
   }
-  return callbacks
+  return callbacks;
 }
 
 describe('Build', function() {
   it('should emit the appropriate events when running a build step', function(done) {
-    var build = new Build()
-    var container = new Container({ docker: null })
-    build.setContainer(container)
-    var step = new Step()
-    build.addStep(step)
-    var callbacks = resolver(5, done)
-    build.on('taskStart', callbacks[0])
-    build.on('taskEnd', callbacks[1])
-    step.on('start', callbacks[2])
-    step.on('end', callbacks[3])
+    var build = new Build();
+    var container = new Container({docker: null});
+    build.setContainer(container);
+    var step = new Step();
+    build.addStep(step);
+    var callbacks = resolver(5, done);
+    build.on('taskStart', callbacks[0]);
+    build.on('taskEnd', callbacks[1]);
+    step.on('start', callbacks[2]);
+    step.on('end', callbacks[3]);
     build.run(callbacks[4]);
-  })
+  });
   it('should stream an event', function(done) {
-    var build = new Build({ id: 1 })
-    var step = new Step()
-    build.addStep(step)
+    var build = new Build({id: 1});
+    var step = new Step();
+    build.addStep(step);
     var callbacks = resolver(2, function() {
-      streamData[0].buildId.should.equal(1)
-      streamData[0].data.should.equal('stdout input line 1')
-      streamData[0].stream.should.equal('stdout')
-      streamData[1].data.should.equal('stdout input line 2')
-      streamData[1].stream.should.equal('stdout')
-      streamData[2].data.should.equal('stderr input line 1')
-      streamData[2].stream.should.equal('stderr')
-      streamData[3].data.should.equal('stderr input line 2')
-      streamData[3].stream.should.equal('stderr')
-      done()
-    })
-    var streamData = []
+      streamData[0].buildId.should.equal(1);
+      streamData[0].data.should.equal('stdout input line 1');
+      streamData[0].stream.should.equal('stdout');
+      streamData[1].data.should.equal('stdout input line 2');
+      streamData[1].stream.should.equal('stdout');
+      streamData[2].data.should.equal('stderr input line 1');
+      streamData[2].stream.should.equal('stderr');
+      streamData[3].data.should.equal('stderr input line 2');
+      streamData[3].stream.should.equal('stderr');
+      done();
+    });
+    var streamData = [];
     var chunkProcessor = function(data, enc, cb) {
-      data.data = data.data.toString()
-      streamData.push(data)
-      cb()
-    }
+      data.data = data.data.toString();
+      streamData.push(data);
+      cb();
+    };
     build
       .getStream()
-      .pipe(through2.obj(chunkProcessor, callbacks[0]))
-    build.run(callbacks[1])
-  })
-})
+      .pipe(through2.obj(chunkProcessor, callbacks[0]));
+    build.run(callbacks[1]);
+  });
+});
