c86108f919cbea80af579ce8ec7aade40111456d
Howard Tyson <howard@howardtyson.com>
2015-12-18 15:36:51 -0800

    Adding a StepList task plugin for executing a set of steps.

---
+++ test/tasks/StepList.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var should = require('should');
+
+var lib = require('../..');
+var StepList = lib.plugins.TaskRunner.StepList;
+var logger = require('../../lib/logger');
+var mockContainer = {log: logger.getLogger()};
+
+var Step = require('../fixtures/TaskPlugin');
+
+describe.only('StepList', function() {
+  it('should run a set of steps passed to the constructor', function(done) {
+    var step1 = new Step(mockContainer);
+    var step2 = new Step(mockContainer);
+    var step3 = new Step(mockContainer);
+    var list = new StepList(mockContainer, {steps: [step1, step2]});
+    list.steps.should.containEql(step1);
+    list.steps.should.containEql(step2);
+    list.steps.should.not.containEql(step3);
+    list.run(function(error) {
+      step1.state.should.equal('completed');
+      step2.state.should.equal('completed');
+      step3.state.should.not.equal('completed');
+      should.not.exist(error);
+      done();
+    });
+  });
+});
