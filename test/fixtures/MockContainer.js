69936217896836b048fcea994215b120e6dc5bf3
Howard Tyson <howard@howardtyson.com>
2015-12-23 07:27:12 -0800

    Adding tests for the AbstractStep that execute the the exec against the mock container and several other simple checks.

--- test/fixtures/MockContainer.js
+++ test/fixtures/MockContainer.js
@@ -1,5 +1,7 @@
 'use strict';
 var bunyan = require('bunyan');
+var through2 = require('through2');
+var Resolver = require('multiple-callback-resolver');
 
 var logger = bunyan.createLogger({
   name: 'probo',
@@ -16,6 +18,31 @@ class Container {
   constructor(options) {
     options = options || {};
     this.log = options.log || logger;
+  }
+
+  exec(command, options, done) {
+    if (typeof options == 'function') {
+      done = options;
+      options = {};
+    }
+    var streams = {
+      stdIn: through2(),
+      stdOut: through2(),
+      stdError: through2(),
+    };
+    setImmediate(this._simulateStream.bind(null, 'stdOut', streams.stdOut));
+    setImmediate(this._simulateStream.bind(null, 'stdError', streams.stdError));
+    var callbacks = Resolver.resolver(2, done);
+    streams.stdOut.on('end', callbacks[0]);
+    streams.stdError.on('end', callbacks[1]);
+    return streams;
+  }
+
+  _simulateStream(name, stream) {
+    for (let i = 1; i <= 10; i++) {
+      stream.write(`${name}: data written ${i}\n`);
+    }
+    stream.end();
   }
 
   stop(done) {
--- test/tasks/AbstractStep.js
+++ test/tasks/AbstractStep.js
@@ -2,13 +2,37 @@
 
 var Resolver = require('multiple-callback-resolver');
 var should = require('should');
+var through2 = require('through2');
 
 var MockContainer = require('../fixtures/MockContainer');
 var mockContainer = new MockContainer();
 
+var lib = require('../..');
+var AbstractStep = lib.plugins.TaskRunner.AbstractPlugin;
 var Step = require('../fixtures/TaskPlugin');
 
 describe('AbstractStep', function() {
+  it('should throw an exceoption if it is used directly', function() {
+    try {
+      new AbstractStep(mockContainer);
+      throw new Error('Instantiation should have failed.');
+    }
+    catch (error) {
+      error.message.should.containEql('Can\'t instantiate abstract class AbstractPlugin');
+      should.exist(error);
+    }
+  });
+  it('should throw an exception if a consuming class does not implement buildCommand', function() {
+    class FailureClass extends AbstractStep {}
+    try {
+      var item = new FailureClass(mockContainer);
+      item.run();
+    }
+    catch (error) {
+      should.exist(error);
+      error.message.should.equal('AbstractPlugin.buildCommand must be implemented by a subclass');
+    }
+  });
   it('should timeout if a step takes too long', function(done) {
     var options = {
       delay: 10,
@@ -24,6 +48,38 @@ describe('AbstractStep', function() {
     });
     step.on('timeout', callbacks[0]);
     step.run(callbacks[1]);
+  });
+  it('should construct the appropriate object stream', function(done) {
+    var stream = [];
+    class ConcreteStep extends AbstractStep {
+      buildCommand() { return []; }
+    }
+    var step = new ConcreteStep(mockContainer);
+    step
+      .getStream()
+      .pipe(through2.obj(function(data, enc, cb) {
+        stream.push(data);
+        cb();
+      }));
+    step.run(function(error) {
+      should.not.exist(error);
+      stream[0].stream.should.equal('stdout');
+      stream[0].data.should.equal('stdOut: data written 1');
+      stream[10].stream.should.equal('stderr');
+      stream[10].data.should.equal('stdError: data written 1');
+      done();
+    });
+  });
+  it('should serialize to json including the appropriate attributes', function() {
+    var step = new Step(mockContainer);
+    var json = step.toJSON();
+    Object.keys(json).length.should.equal(step._jsonAttributes.length);
+  });
+  it('should calculate the ellapsed time', function() {
+    var step = new Step(mockContainer);
+    step.startTime = 5;
+    step.endTime = 10;
+    step.ellapsedTime.should.equal(5);
   });
 });
 
