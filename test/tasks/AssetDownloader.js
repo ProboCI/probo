91a068a5c8e7125d3b17172a03eb702496304a16
Ilya Braude <ilya@bluenexa.com>
2016-01-03 00:17:09 -0500

    API key auth support for downloading assets from Assets server

--- test/tasks/AssetDownloader.js
+++ test/tasks/AssetDownloader.js
@@ -1,21 +1,41 @@
 'use strict';
 var AssetDownloader = require('../../lib/plugins/TaskRunner/AssetDownloader');
 
+var sinon = require('sinon');
+
 var mockContainer = {
   log: {child: function() {}},
 };
 
 describe('AssetDownloader', function() {
+  var options = {
+    url: 'http://asset.server',
+    bucket: 'bucket-name',
+    assets: [
+      'db.tgz',
+      {files: 'files.tgz'},
+    ],
+  };
+
+  beforeEach(function() {
+    sinon.spy(AssetDownloader.prototype, 'setScript');
+  });
+
+  afterEach(function() {
+    AssetDownloader.prototype.setScript.restore();
+  });
+
   it('builds proper task configuration', function() {
-    var options = {
-      assetServerUrl: 'http://asset.server',
-      assetBucket: 'bucket-name',
-      assets: [
-        'db.tgz',
-        {files: 'files.tgz'},
-      ],
-    };
     var ad = new AssetDownloader(mockContainer, options);
+
+    // ensure that AssetDownloader is creating the correct script
+    // (first call to setScript happens in the Script constructor)
+    ad.setScript.getCall(1).args[0].should.eql([
+      'mkdir -p $ASSET_DIR',
+      'cd $ASSET_DIR',
+      'wget -nv -O db.tgz  http://asset.server/asset/bucket-name/db.tgz',
+      'wget -nv -O files.tgz  http://asset.server/asset/bucket-name/files',
+    ]);
 
     ad.script.should.eql(`unset HISTFILE
 export PS4='$ '
@@ -23,9 +43,29 @@ set -x
 mkdir -p $SRC_DIR; cd $SRC_DIR
 mkdir -p $ASSET_DIR
 cd $ASSET_DIR
-wget -nv -O db.tgz http://asset.server/asset/bucket-name/db.tgz
-wget -nv -O files.tgz http://asset.server/asset/bucket-name/files
+wget -nv -O db.tgz  http://asset.server/asset/bucket-name/db.tgz
+wget -nv -O files.tgz  http://asset.server/asset/bucket-name/files
 `);
+
+    ad.description().should.eql('AssetDownloader db.tgz, files');
+  });
+
+  it('builds proper task configuration with auth', function() {
+    options.token = 'tok';
+    var ad = new AssetDownloader(mockContainer, options);
+    delete options.token;
+
+    ad.setScript.getCall(1).args[0].should.eql([
+      'mkdir -p $ASSET_DIR',
+      'cd $ASSET_DIR',
+      `wget -nv -O db.tgz --header='Authorization:Bearer tok' http://asset.server/asset/bucket-name/db.tgz`,
+      `wget -nv -O files.tgz --header='Authorization:Bearer tok' http://asset.server/asset/bucket-name/files`,
+    ]);
+
+    ad.options.secrets.should.eql([
+      'bucket-name',
+      'tok',
+    ]);
 
     ad.description().should.eql('AssetDownloader db.tgz, files');
   });
