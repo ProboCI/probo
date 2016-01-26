f7935133b127847b90e85dd3c12d46fcd86e254c
Howard Tyson <howard@howardtyson.com>
2016-01-26 13:36:27 -0800

    Adding a cli test.

---
+++ test/cli.js
@@ -0,0 +1,15 @@
+'use strict';
+var should = require('should');
+var lib = require('..');
+var cli = lib.cli;
+
+describe('cli loader', function() {
+  it('should load all relevant commands', function(done) {
+    cli.loadCommands(function(error, data) {
+      should.exist(data.help);
+      done(error);
+    });
+  });
+});
+
+
